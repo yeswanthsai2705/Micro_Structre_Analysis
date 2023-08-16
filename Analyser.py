bf224cf2fe77ef9fd0fb9653ded1943e2d64c800
Yeswanth sai yadla <94543732+yeswanthsai2705@users.noreply.github.com>
2023-08-16 22:58:58 +0530

    Add files via upload

---
+++ Analyser.py
@@ -0,0 +1,25 @@
+# Import necessary libraries
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+# Load and preprocess data
+data = pd.read_csv('microstructural_data.csv')  # Replace with your dataset
+X = data[['Grain_Size', 'Phase_Fractions']]   # Features
+y = data['Mechanical_Properties']              # Target
+
+# Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize and train the machine learning model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions on the test data
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+print(f"Mean Squared Error: {mse}")
